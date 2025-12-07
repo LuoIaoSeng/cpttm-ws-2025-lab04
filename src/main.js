91e25eabc7fe084e0de086eb3fb4f6c9fbdbcf72
LUO IAO SENG <luoyoucheng991528@gmail.com>
2025-12-07 13:07:35 +0800

    user custom input

--- src/main.js
+++ src/main.js
@@ -7,25 +7,24 @@ const game = {
     tempIndex: [],
     checkNum: -1,
     counter: 0,
-    waitTime: 3,
+    numbers: 8,
+    repeatedTime: 4,
+    prepareTime: 3,
     init: function () {
-        for (let i = 0; i < 4; i++) {
-            for (let j = 0; j < 9; j++) {
+
+        for (let i = 0; i < this.repeatedTime; i++) {
+            for (let j = 0; j <= this.numbers; j++) {
                 this.answers.push(j)
             }
         }
 
         this.answers = this.answers.sort(() => Math.random() - 0.5)
 
-        for (let i = 0; i < 36; i++) {
-
-            const childDiv = document.createElement('div')
-            // childDiv.setAttribute('data-index', i)
-            childDiv.setAttribute('data-answer', this.answers[i])
-            childDiv.classList = 'item'
-            childDiv.innerText = this.answers[i]
-            this.dom.appendChild(childDiv)
-        }
+        document.querySelectorAll('.item').forEach((d, i) => {
+            
+            d.setAttribute('data-answer', this.answers[i])
+            d.innerText = this.answers[i]
+        })
 
         setTimeout(() => {
             document.querySelectorAll('.item').forEach((dom) => {
@@ -34,12 +33,11 @@ const game = {
                     this.clickDom(dom)
                 })
             })
-        }, this.waitTime * 1000)
+        }, this.prepareTime * 1000)
     },
     clickDom: function (dom) {
-        // const index = dom.getAttribute('data-index')
         const value = dom.getAttribute('data-answer')
-        if(this.temp.indexOf(dom) != -1) return
+        if (this.temp.indexOf(dom) != -1) return
         dom.innerText = value
         this.temp.push(dom)
         if (this.checkNum == -1) {
@@ -74,4 +72,33 @@ const game = {
     }
 }
 
-game.init()
+function initDom(numbers, repeatedTime) {
+    game.dom.innerHTML = ''
+
+    for (let i = 0; i < (numbers + 1) * repeatedTime; i++) {
+
+        const childDiv = document.createElement('div')
+        childDiv.classList = 'item'
+        game.dom.appendChild(childDiv)
+    }
+}
+
+document.querySelector('.numbers').addEventListener('change', (e) => {
+    game.numbers = parseInt(e.target.value)
+    initDom(game.numbers, game.repeatedTime)
+})
+
+document.querySelector('.repeatTime').addEventListener('change', (e) => {
+    game.repeatedTime = parseInt(e.target.value)
+    initDom(game.numbers, game.repeatedTime)
+})
+
+document.querySelector('.prepareTime').addEventListener('change', (e) => {
+    game.prepareTime = parseInt(e.target.value)
+})
+
+document.querySelector('.start').addEventListener('click', () => {
+    game.init()
+})
+
+initDom(game.numbers, game.repeatedTime)
